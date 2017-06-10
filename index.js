a74bd9eeeaf055a7422b4554079219a854b70cc7
Manuel Enrique <menrique.gutierrez@gmail.com>
2017-06-09 21:32:36 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let request = require('request')
+
+// Code Block Inputs
+const inputs = {
+};
+
+let p = new Promise(function(resolve, reject) {
+
+// add the code to test here.. 
+
+
+
+// End of promise
+}) 
+
+// Output of the code.
+p.then((val) => console.log("fulfilled:", val))
+.catch((err) => console.log("rejected:", err))
